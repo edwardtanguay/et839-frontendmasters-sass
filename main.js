2bf9b5a387217df8925901e5ae4ff30bdced1d3d
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-18 02:21:55 +0100

    before restructure

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-import './style.scss'
+import './style.scss';
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('#app').innerHTML = /*html*/ `
 <h1>Sass Course notes</h1>
-<p>testing</p>
+<main class="highlighted">
+	<h2>Section 1</h2>
+		<p class="highlighted">test highlighted</p>
+		<div class="highlightedBox">highlighted box</div>
+		<p class="test001">This is saturated 0%.</p>
+		<p class="test002">This is saturated 100%.</p>
+		<p class="test003">This is darkened 10%.</p>
+		<p class="test004">This is darkened 20%.</p>
+		<p class="test005">This is darkened 30%.</p>
+</main>
 `;
